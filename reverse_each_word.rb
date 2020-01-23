0ce26651220ba66c192e6fb38448403046536c22
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-23 09:21:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 $Phrase = "Reverse The Words In This Sentence"
 
 def reverse_each_word(phrase)
-phrase.each do |word|
+phrase.each do |phrase|
   phrase.reverse
 end
 
 def reverse_each_word(phrase)
-phrase.collect do |word|
+phrase.collect do |phrase|
 phrase.reverse
 end
